25459d128d5d7b4f98bb52054e09ed68fa57262e
GinaHolby <gina.s.holby@gmail.com>
2023-10-30 11:27:51 +0100

    g

--- app/src/lib/utils/sanity.ts
+++ app/src/lib/utils/sanity.ts
@@ -3,15 +3,15 @@ import { createClient } from '@sanity/client';
 import type { ImageAsset, Slug } from '@sanity/types';
 import groq from 'groq';
 
-import { PUBLIC_SANITY_DATASET, PUBLIC_SANITY_PROJECT_ID } from '$env/static/public';
+/* import { PUBLIC_SANITY_DATASET, PUBLIC_SANITY_PROJECT_ID } from '$env/static/public'; */
 
-if (!PUBLIC_SANITY_PROJECT_ID || !PUBLIC_SANITY_DATASET) {
+/* if (!PUBLIC_SANITY_PROJECT_ID || !PUBLIC_SANITY_DATASET) {
 	throw new Error('Did you forget to run sanity init --env?');
-}
+} */
 
 export const client = createClient({
-	projectId: PUBLIC_SANITY_PROJECT_ID,
-	dataset: PUBLIC_SANITY_DATASET,
+	projectId: "yz3kgj6k",
+	dataset: "production",
 	useCdn: false, // `false` if you want to ensure fresh data
 	apiVersion: '2023-03-20' // date of setup
 });
